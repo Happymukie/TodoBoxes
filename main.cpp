0f7d6e13be72cbb15910aa7b4ccee0d37637cbf9
Happymukie <1378936941@qq.com>
2022-09-02 11:37:57 +0800

    Constructure

--- main.cpp
+++ main.cpp
@@ -1,7 +1,79 @@
 #include<iostream>
 using namespace std;
+
+const int MAPX = 30;
+const int MAPY = 30;
+
+int Map[MAPX][MAPY];
+
+class Game
+{
+    public:
+        void init();
+        void drawMap();
+};
+
+void Game::drawMap()
+{
+    for (int i = 0; i < MAPX; i++)
+    {
+        for (int j = 0; j < MAPY; j++)
+        {
+            cout << Map[i][j];
+        }
+        cout << endl;
+    }
+}
+
+void Game::init() // display
+{
+    
+    Game::drawMap();
+}
+
+class Object
+{
+    public:
+        int x;
+        int y;
+
+};
+
+class Box: public Object
+{
+    public:
+        void moved() // change coordinate and map then display()
+        {
+            
+        }
+};
+
+class Me: public Object
+{
+    public:
+        void move() // detect and change coordinate and map then Box::moved()
+        {
+
+        }
+};
+
+
+
+void moveit() // detect keyboard then Me::move()
+{
+
+
+}
+
 int main()
 {
-    cout<<"test";
+    Game game;
+    game.init();
+
+    while(1)
+    {
+        moveit();
+    }
+
     return 0;
 }
